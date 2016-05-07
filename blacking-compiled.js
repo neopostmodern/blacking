82cdc3b650609bdeddaa65011824fabc1dd59c12
neopostmodern <clemens@neopostmodern.com>
2016-05-07 12:08:08 +0200

    Added Array.includes polyfill

--- blacking-compiled.js
+++ blacking-compiled.js
@@ -1,12 +1,45 @@
 "use strict";
 
-/**
- * @return {string}
- */
+// POLYFILL
 
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+if (!Array.prototype.includes) {
+  Array.prototype.includes = function (searchElement /*, fromIndex*/) {
+    'use strict';
+
+    var O = Object(this);
+    var len = parseInt(O.length) || 0;
+    if (len === 0) {
+      return false;
+    }
+    var n = parseInt(arguments[1]) || 0;
+    var k;
+    if (n >= 0) {
+      k = n;
+    } else {
+      k = len + n;
+      if (k < 0) {
+        k = 0;
+      }
+    }
+    var currentElement;
+    while (k < len) {
+      currentElement = O[k];
+      if (searchElement === currentElement || searchElement !== searchElement && currentElement !== currentElement) {
+        // NaN !== NaN
+        return true;
+      }
+      k++;
+    }
+    return false;
+  };
+}
+
+/**
+ * @return {string}
+ */
 var Blacking = function Blacking(text, user) {
   var regex = /\[([^\]]*)\]\{([^\}]+)\}/g;
   var match = void 0;
